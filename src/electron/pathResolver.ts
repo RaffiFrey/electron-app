6163882813b18e6bf0f5764c616e49aa79a26814
Raphael Frey <159828193+RaffiFrey@users.noreply.github.com>
2025-06-30 19:45:03 +0200

    Add preload script and update main window configuration

---
+++ src/electron/pathResolver.ts
@@ -0,0 +1,11 @@
+﻿import path from 'path';
+import {app} from 'electron';
+import {isDev} from './util.js';
+
+export function getPreloadPath(): string {
+  return path.join(
+    app.getAppPath(),
+    isDev() ? '.' : '..',
+    '/dist-electron/preload.cjs'
+  );
+}
